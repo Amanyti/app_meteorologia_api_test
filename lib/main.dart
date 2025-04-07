5c5178a9d531a60fec367e1b4d1a3b6782b7371e
Amanyti <amandita.queiroz2142@gmail.com>
2025-04-07 17:37:01 -0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,164 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http show get;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController controllerText = TextEditingController();
+  final String apiKey =
+      '05ca5fde469800d20c0a3fb08b17c2b3'; // Substitua com sua chave da API
+  String city = '';
+  String temperature = '';
+  String description = '';
+
+  bool button = false;
+
+  Future<void> getWeather() async {
+    city = controllerText.text;
+    final url = Uri.parse(
+      'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey&units=metric&lang=pt_br',
+    );
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      setState(() {
+        temperature = data['main']['temp'].toString();
+        description = data['weather'][0]['description'];
+      });
+    } else {
+      setState(() {
+        city = 'Erro ao encontrar a cidade';
+        temperature = '';
+        description = '';
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getWeather(); // Chama a função ao iniciar
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[100],
+        title: Text('Previsão do Tempo', style: TextStyle(fontSize: 50)),
+        centerTitle: true,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/18220838_v1015-101a.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+
+        child: Align(
+          alignment: Alignment(0, 0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 400,
+                child: TextField(
+                  controller: controllerText,
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                  decoration: InputDecoration(
+                    labelText: 'Digite o nome da cidade...',
+                    labelStyle: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    button = true;
+                    getWeather();
+                  });
+                },
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 70, vertical: 10),
+                  backgroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(7),
+                  ),
+                ),
+                child: Text(
+                  'Exibir',
+                  style: TextStyle(fontSize: 20, color: Colors.black),
+                ),
+              ),
+              SizedBox(height: 20),
+
+              (button)
+                  ? Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+
+                    child: Column(
+                      children: [
+                        Text(
+                          'Cidade: $city',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 16),
+                        Text(
+                          'Temperatura: $temperature °C',
+                          style: TextStyle(fontSize: 20),
+                        ),
+
+                        Text(
+                          'Descrição: $description',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        SizedBox(height: 20),
+                      ],
+                    ),
+                  )
+                  : SizedBox(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
